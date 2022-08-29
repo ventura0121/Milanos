91e0394a87c0675f04d24d7d9078ae67a4386d39
ventura0121 <ventura0121@gmail.com>
2022-08-29 18:21:27 -0400

    Completed NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,64 +1,31 @@
-import React from 'react'
-import logo from '../logo.jpeg'
-import {Toolbar, Typography} from '@material-ui/core'
-import {makeStyles} from "@material-ui/core/styles"; 
-import CreateButton from './Button'
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import logo from '../components/logo.jpeg'
 
-const styles = makeStyles({
-    bar:{
-        paddingTop: "1.15rem",
-        backgroundColor: "#fff",
-        '@media (max-width:780px)': { 
-            flexDirection: "column"
-            }
-    },
-    logo: {
-        width: "15%", 
-        '@media (max-width:780px)': { 
-            display: "none"
-            }
-    },
-    logoMobile:{
-        width: "100%", 
-        display: "none", 
-        '@media (max-width:780px)': { 
-            display: "inline-block"
-            }
-    },
-    menuItem: {
-        cursor: "pointer", 
-        flexGrow: 1,
-        "&:hover": {
-            color:  "#4f25c8"
-        },
-        '@media (max-width:780px)': { 
-            paddingBottom: "1rem"    }
-    }
-})
-
-function NavBar() {
-    const classes = styles()
-    return (
-            <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>   
-                <img src={logo} className={classes.logo}/> 
-                <Typography variant="h6" className={classes.menuItem}>
-                    HOME
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
-                    SERVICES
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
-                    ABOUT
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
-                    PROJECTS 
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
-                    CONTACT
-                </Typography>
-                <CreateButton txt="Fancy Button"/>
-            </Toolbar>
-    )
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        <img src={logo} style = {{height: 120, width: 120}} alt="website logo"/>
+      </Link>
+      <ul>
+        <CustomLink to="/Services">Services</CustomLink>
+        <CustomLink to="/About">About</CustomLink>
+        <CustomLink to="/Projects">Projects</CustomLink>
+        <CustomLink to="/Contact">Contact</CustomLink>
+      </ul>
+    </nav>
+  )
 }
 
-export default NavBar
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
