cc3847900da8379b8f8d5c77be1c2273acf6e723
ventura0121 <ventura0121@gmail.com>
2022-08-30 02:40:48 -0400

    Copied code over from duplicating project before restoring it. Back at it babygit add .

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
-import Rebar from '../components/Images/Rebar.jpg'
-import "./Home.css"
 import React, { Component }  from 'react';
+//import Rebar from '../components/Images/Rebar.jpg'
+import "./Home.css"
 
 export default function Home() {
     return (
     <div className="welcome-container">
     <div className="welcome-img-container">
-      <img src={Rebar} alt="Rebar.jpg" id="Rebar.jpg" />
+        
+        {/* <div style ="background-image: url('../components/Images/Rebar.jpg');"></div> */}
+        {/* <img src={Rebar} alt="Rebar.jpg" id="Rebar.jpg" /> */}
     </div>
     <h2 className="welcome-text">
         Quality in Every Project
