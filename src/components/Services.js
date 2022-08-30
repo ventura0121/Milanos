68f2837fdc0016f823f3cdd18763a63a30154b28
ventura0121 <ventura0121@gmail.com>
2022-08-30 06:55:26 -0400

    I have pictures in

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,15 +1,19 @@
 import "./Services.css"
+import CompactedSoil from "./Images/CompactedSoil.jpg"
+import ConcretePour from "./Images/ConcretePour.jpg"
+import ConcretePour2 from "./Images/ConcretePour2.jpg"
+import Demo from "./Images/Demo.png"
+import Fill from "./Images/Fill.jpg"
+import FinishedConc from "./Images/FinishedConc.jpg"
+import Foundation from "./Images/Foundation.jpg"
+import Pit from "./Images/Pit.jpg"
+import Rebar from "./Images/Rebar.jpg"
 
 export default function Services() {
     return (
         <div className="Services-container">
-        <div className="Services-img-container">
-          {/* <img src={Rebar} alt="Rebar.jpg" id="Rebar.jpg" /> */}
-        </div>
         <h1>Here at Milano's we provide only the best in:</h1>
         <h2 className="Services-text">
-            
-            
             <ul>
             <li>Chimneys & Fireplaces</li>
             <li>Brick Laying & Pointing</li>
@@ -26,10 +30,19 @@ export default function Services() {
             <li>Excavation</li>
             <li>Demolition</li>
             </ul>
-
-
         </h2>
-
+        <h2>See some of the company highlights below!</h2>
+        <div id="img-wrapper">
+            <div><img src={Rebar} alt="Rebar.jpg" id="Rebar.jpg" /></div>
+            <div><img src={CompactedSoil} alt="CompactedSoil.jpg" id="CompactedSoil.jpg" /></div>
+            <div><img src={ConcretePour} alt="ConcretePour.jpg" id="ConcretePour.jpg" /></div>
+            <div><img src={ConcretePour2} alt="ConcretePour2.jpg" id="ConcretePour2.jpg" /></div>
+            <div><img src={Demo} alt="Demo.jpg" id="Demo.jpg" /></div>
+            <div><img src={Fill} alt="Fill.jpg" id="Fill.jpg" /></div>
+            <div><img src={FinishedConc} alt="FinishedConc.jpg" id="FinishedConc.jpg" /></div>
+            <div><img src={Foundation} alt="Foundation.jpg" id="Foundation.jpg" /></div>
+            <div><img src={Pit} alt="Pit.jpg" id="Pit.jpg" /></div>
+        </div>
       </div>
     );
   }
