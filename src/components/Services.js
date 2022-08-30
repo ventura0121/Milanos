a734826376fe46f7519caddaa696e16fb7d620af
ventura0121 <ventura0121@gmail.com>
2022-08-30 06:23:20 -0400

    Completed list in Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,3 +1,4 @@
+import "./Services.css"
 
 export default function Services() {
     return (
@@ -5,24 +6,28 @@ export default function Services() {
         <div className="Services-img-container">
           {/* <img src={Rebar} alt="Rebar.jpg" id="Rebar.jpg" /> */}
         </div>
+        <h1>Here at Milano's we provide only the best in:</h1>
         <h2 className="Services-text">
-            Here at Milano's we provide only the best in:
+            
             
             <ul>
             <li>Chimneys & Fireplaces</li>
             <li>Brick Laying & Pointing</li>
             <li>Stairs & Walkways</li>
             <li>Patios</li>
+            <br/>
             <li>Cultured Stone</li>
+            <li>Retaining Walls</li>
             <li>Concrete Work</li>
             <li>Foundations</li>
+            <br/>
             <li>Driveways</li>
             <li>Pavers</li>
             <li>Excavation</li>
             <li>Demolition</li>
             </ul>
 
-            
+
         </h2>
 
       </div>
